e964bf35c3a3845c08b8600c1cc7b54f6994d904
dimstr <93518175+dimstr@users.noreply.github.com>
2022-07-17 17:16:02 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 $(function() {
-    $('.img').load(function() {
-       $(this).parent().find(".loading").hide();
-    });
+	if(navigator.userAgent.match(/msie/i) || navigator.userAgent.match(/trident/i)) {
+		$('.loading').hide();
+	}
+	$('.image').load(function() {
+		$(this).parent().find(".loading").hide();
+	});
 });
 alert("hello");
